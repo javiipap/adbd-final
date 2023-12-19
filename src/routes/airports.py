3712d969b8ff2763c13001e8700a593d69c2cff7
aliciagcp <alu0101420868@ull.edu.es>
2023-12-19 23:49:31 +0000

    airports and bonifications

--- src/routes/airports.py
+++ src/routes/airports.py
@@ -1,8 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from ..db import get_cursor
 
 airports = Blueprint('airports', __name__, url_prefix='/airports')
 
 
-@airports.route('')
+@airports.route('', methods=['GET', 'POST'])
 def hello():
-    return 'Hello, airports!'
+    # return 'Hello, airports!'
+    if request.method == 'GET':
+        cursor = get_cursor()
+        cursor.execute('SELECT name, country, city, iata FROM airports;')
+        output = cursor.fetchall()
+        return jsonify(output)
+
+    if request.method == 'POST':
+        airport_data = request.json
+        cursor = get_cursor()
+
+        required_fields = ['name', 'country','city','iata']
+        if not all(field in airport_data for field in required_fields):
+            return jsonify({'error': 'Missing required fields'}), 400
+
+        cursor = get_cursor()
+        cursor.execute('INSERT INTO airports (name, country, city, iata) VALUES (%s, %s, %s, %s);',
+                       (airport_data['name'], airport_data['country'], airport_data['city'], airport_data['iata']))
+
+        cursor.connection.commit()
+        cursor.close()
+
+        return jsonify({'message': 'Airport created successfully'}), 201
