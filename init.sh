25eaf962c4e94c5fb152767f500fe8b3ae56410e
javiipap <jpiopadilla@gmail.com>
2023-12-19 10:45:07 +0000

    Update init.sh script to display messages when not in virtual environment

--- init.sh
+++ init.sh
@@ -7,9 +7,11 @@ if [ ! -d "env" ]; then
 fi
 
 if [ "x$(which python)" != "x$VIRTUAL_ENV/bin/python" ]; then
-  echo "You are not in the virtual environment. Run 'source env/bin/activate' to activate it."
+  echo "\033[1;33m-------------------------------"
+  echo "You are not in the virtual environment. Run 'source env/bin/activate' to activate it.\033[0m"
   . env/bin/activate
-  echo "Virtual environment activated only for script execution."
+  echo "\033[0;33mVirtual environment activated only for script execution."
+  echo "-------------------------------\033[0m"
 fi
 
 python main.py
